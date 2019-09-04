911ad39bc22fe29f994dad088aa866fce0a1ee77
Aananya <anan@Anans-Work-Mac.local>
2019-09-04 23:40:52 +0530

    v_02

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='gluestring',
     packages=['gluestring'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='Easy way to create template strings',
     author='Aananya',
     author_email='astream.h@outlook.com',
     url='https://github.com/aananya27/gluestring',
-    download_url='https://github.com/aananya27/gluestring/archive/v_01.tar.gz',
+    download_url='https://github.com/aananya27/gluestring/archive/v_02.tar.gz',
     keywords=['template', 'string-formatter', 'util'],
     install_requires=[],
     classifiers=[
