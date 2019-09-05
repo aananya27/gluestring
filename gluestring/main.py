60b27111e81fdc978edb193131a9dcf28fc748a8
Aananya <anan@Anans-Work-Mac.local>
2019-09-06 01:12:46 +0530

    v_03

--- gluestring/main.py
+++ gluestring/main.py
@@ -5,13 +5,11 @@ def glue_it(templateString, dictionaryToMatch):
     number_of_patterns_found = len(pattern.findall(templateString))
 
     for i in range(number_of_patterns_found):
-        
         val = pattern.search(templateString).groups()[0]
 
         if val.strip() in dictionaryToMatch:
             templateString = templateString.replace('{{'+val+'}}',dictionaryToMatch[val.strip()])
         else:
-            templateString = templateString.replace('{{'+val+'}}',dictionaryToMatch['default'])  
-    # print(templateString)          
+            templateString = templateString.replace('{{'+val+'}}',dictionaryToMatch['default'])         
     return templateString
 
